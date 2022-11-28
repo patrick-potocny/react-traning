c6d057718204434879a9b90659faf36876b17254
patrick-potocny <pato.potocny85@gmail.com>
2022-11-28 18:32:10 +0100

    Finished simple tasks app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import Overview from './components/Overview'
+import React, { Component } from 'react';
+import uniqid from 'uniqid'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      task: {text: '', id: uniqid()},
+      tasks: []
+    }
+
+    this.onSubmitHandler = this.onSubmitHandler.bind(this)
+    this.onChangeHandler = this.onChangeHandler.bind(this)
+    this.delFunc = this.delFunc.bind(this)
+  }
+
+  onSubmitHandler(event) {
+    event.preventDefault();
+    this.setState({
+      task: {text: '', id: uniqid()},
+      tasks: this.state.tasks.concat(this.state.task)
+    })
+  }
+
+  onChangeHandler(event) {
+    this.setState({
+     task: {text: event.target.value,
+            id: this.state.task.id},
+    })
+  }
+
+  delFunc(event) {
+    const indexToDel = this.state.tasks.findIndex(task => task.id === event.target.id)
+    this.setState(this.state.tasks.splice(indexToDel, 1))
+  }
+
+  render() {
+    const {task, tasks} = this.state
+
+    return <div className="App">
+      <form onSubmit={this.onSubmitHandler}>
+        <label htmlFor="task"><strong>Task </strong></label>
+        <input type="text" placeholder="Enter Task" onChange={this.onChangeHandler} name="task" value={task.text} required/>
+        <button type="submit">Add +</button>
+        <Overview tasks={tasks} delFunc={this.delFunc} />
+      </form>
     </div>
-  );
+  }
 }
 
 export default App;

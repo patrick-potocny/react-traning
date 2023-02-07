e8dd60c482e5d8b45ddfefbfe9477a35450e1636
patrick-potocny <patrik.potocny85@gmail.com>
2023-02-07 17:47:18 +0100

    NextJS blog

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First post</title>
+      </Head>
+      <h1>First Post</h1>
+    </>
+  );
+}
